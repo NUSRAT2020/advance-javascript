5d91a834eef7bc88ff63a412326a80b338bb9ef2
NUSRAT2020 <ipaazom@gmail.coom>
2020-11-19 22:12:30 -0500

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// truthy
+// 1/ " "
+// 2/ "0"
+// 3/ any number without 0 
+// 4/ every String is true other
+//  then empty String
+// 5/ []
+
+// falsy
+// 1/""
+// 2/ undefined
+// 3/ null
+// 4/ NaN
+// 5/ value - false
+var age = " ";
+ if (age ) {console.log( "condition is true");
+         
+ } else {
+         console.log( "condition is false");
+ }
