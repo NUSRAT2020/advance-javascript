03fed71068a4bec240a2e38b350d2227b9b05ec0
NUSRAT2020 <ipaazom@gmail.coom>
2020-11-25 23:26:01 -0500

    map-vs-filter

--- error.js
+++ error.js
@@ -1,15 +1,15 @@
-// let paki;
-// console.log(paki);
-// function add(num1,num2) { console.log(num1 + num2);
-//   return  ; }     
+let paki;
+console.log(paki);
+function add(num1,num2) { console.log(num1 + num2);
+  return  ; }     
 
-// const result = add(30,32);
-//  console.log(result);
+const result = add(30,32);
+ console.log(result);
 
-//  function add(num1,num2){ 
-//           console.log(num1, num2)
-//  } 
-//  const result = add(12);
-//  console.log(num1,num2);
+ function add(num1,num2){ 
+          console.log(num1, num2)
+ } 
+vg const result = add(12);
+console.log(num1,num2);
 let fun = undefined;
- null (in short empty / no value set)
+null (iv cn short empty / no value set)
---
+++ map-filter.js
@@ -0,0 +1,19 @@
+const numbers = [2, 3, 5, 8, 60, 49];
+//  const output = [];
+// for (let i = 0; i < numbers.length; i++) {
+//         const element = numbers[i];
+//         const result = element * element ;
+//         output.push(result);}
+// console.log(output);
+//   function square (element) {
+//         return element * element
+        
+// } const square = element => element * element
+//  const square = x => x*x
+  
+
+//   const result = numbers.map( x=> x*x
+        
+// ) 
+ const bigger =numbers.filter( x => x < 5)
+console.log(bigger);
